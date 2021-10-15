6860b7209399749b0beae8498654f074fac21b3a
inga000 <90465707+inga000@users.noreply.github.com>
2021-10-15 17:17:11 +0200

    Delete command.sh

--- InputFileExamples/AutomaticSizing/command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-{file path to acst}/acst/build/bin/acst.sh --log-level-console DEBUG  --circuit-netlist cascodedSymmetricalCMOSOTA.hspice   --output-file cascodedSymmetricalCMOSOTA.xml --analysis automaticsizing --xml-structrec-library-file {file path to acst}/acst/StructRec/xml/AnalogLibrary.xml --hspice-mapping-file HSpiceMapping.xcat --hspice-supplynet-file supplyNets.xcat --device-types-file deviceTypes.xcat --xml-technologie-file TechnologyFile.xml --xml-circuit-information-file CircuitParameterAndSpecifications.xml --transistor-model SHM --scaling 0.1mum --runtime 5
-
